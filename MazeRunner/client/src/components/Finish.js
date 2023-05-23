3c48a8d60600481d55b73c960847d106446bd13d
PavanKumar <66913595+ascender1729@users.noreply.github.com>
2023-05-23 11:40:53 +0530

    Create Finish.js

---
+++ MazeRunner/client/src/components/Finish.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Finish = () => {
+  return (
+    <div className="finish-line">
+      <h2>Finish Line</h2>
+      <p>Congratulations! You made it to the finish line!</p>
+    </div>
+  );
+};
+
+export default Finish;
